25f928d4b4d9d5c7fe9957e2181fa7ec932b6467
KovalVladyslav <vladxnos@gmail.com>
2023-05-14 14:37:27 +0300

    task done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,60 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import css from './App.module.css';
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
+
+let total = 0;
+let positivePercentage = 0;
+
+const options = ['Good', 'Neutral', 'Bad'];
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleClick = value => {
+    value = value.toLowerCase();
+    this.setState(prevState => {
+      return {
+        [value]: prevState[value] + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = () => {
+    total = this.state.good + this.state.neutral + this.state.bad;
+    return total;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    positivePercentage = total && Math.round((this.state.good * 100) / total);
+    return positivePercentage;
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <div className={css.block}>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={options}
+            onLeaveFeedback={this.handleClick}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositiveFeedbackPercentage()}
+          />
+        </Section>
+      </div>
+    );
+  }
+}
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,15 @@
+import propTypes from 'prop-types';
+
+export const Section = ({ title, children }) => {
+  return (
+    <>
+      <h2>{title}</h2>
+      {children}
+    </>
+  );
+};
+
+Section.propTypes = {
+  title: propTypes.string.isRequired,
+  children: propTypes.node.isRequired,
+};
